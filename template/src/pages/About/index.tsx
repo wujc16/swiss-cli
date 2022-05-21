33042672fcdfa2d42224c02033e44fcb9db0a07c
jinchao.wu <jinchao.wu@bytedance.com>
2022-05-21 16:19:30 +0800

    feat: 完成 sass 支持

--- template/src/pages/About/index.tsx
+++ template/src/pages/About/index.tsx
@@ -1,8 +1,17 @@
+/*
+ * @Author: jinchao.wu@bytedance.com
+ * @Date: 2022-01-20 16:28:25
+ * @LastEditTime: 2022-05-21 15:42:06
+ * @LastEditors: jinchao.wu@bytedance.com
+ * @Description: 
+ * @FilePath: /github-swiss-cli/template/src/pages/About/index.tsx
+ */
 import React from 'react';
+import styles from './index.css';
 
 export default function About() {
   return (
-    <div>
+    <div className={styles.about}>
       About
     </div>
   );
--- template/webpack/webpack.config.prod.js
+++ template/webpack/webpack.config.prod.js
@@ -1,6 +1,15 @@
 const { merge } = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.config');
+const path = require('path');
+const fs = require('fs');
+
+// 之前先删除掉 dist 文件夹以及里面所有的东西
+const outputPath = path.resolve(__dirname, '../dist');
+fs.rmdirSync(outputPath, { recursive: true });
 
 module.exports = merge(baseWebpackConfig, {
   mode: 'production',
+  output: {
+    path: outputPath,
+  }
 });
