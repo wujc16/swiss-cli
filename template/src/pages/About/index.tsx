4a732bb0ec3d42a18f4845562d2de5e37bda4b02
jinchao.wu <jinchao.wu@bytedance.com>
2022-05-21 16:36:12 +0800

    feat: 删除 fileheader

--- template/src/pages/About/index.tsx
+++ template/src/pages/About/index.tsx
@@ -1,11 +1,3 @@
-/*
- * @Author: jinchao.wu@bytedance.com
- * @Date: 2022-01-20 16:28:25
- * @LastEditTime: 2022-05-21 16:34:22
- * @LastEditors: jinchao.wu@bytedance.com
- * @Description: 
- * @FilePath: /github-swiss-cli/template/src/pages/About/index.tsx
- */
 import React from 'react';
 import './index.css';
 
